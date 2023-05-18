c3e3c52455c198f3e31049147e85b3379c1f6c2b
vfs <vischijau@gmail.com>
2023-05-18 17:54:41 -0300

    Created new components

---
+++ components/CenterMessage.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { Text, View, StyleSheet } from 'react-native'
+
+
+const CenterMessage = ({ message }) => (
+	console.log("CENTER MESSAGE"),
+	<View style={styles.emptyContainer}>
+		<Text style={styles.message}>
+			{message}
+		</Text>
+	</View>
+)
+
+
+const styles = StyleSheet.create({
+	emptyContainer: {
+		padding: 10,
+		borderBottomWidth: 2,
+		borderBottomColor: '#1976D2',
+	},
+	message: {
+		alignSelf: 'center',
+		fontSize: 20,
+	},
+})
+
+
+export default CenterMessage
