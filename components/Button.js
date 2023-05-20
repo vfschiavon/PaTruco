793141f508109076eb74d6c1f9a0adb35a4ebcb7
vfs <vischijau@gmail.com>
2023-05-20 15:05:31 -0300

    Changed style

--- components/Button.js
+++ components/Button.js
@@ -40,7 +40,6 @@ const styles = StyleSheet.create({
 		width: 25,
 		height: 25,
 		marginTop: 100,
-		textAlign: 'center',
 		justifyContent: 'center',
 	},
 	buttonAdd: {
@@ -52,7 +51,7 @@ const styles = StyleSheet.create({
 	buttonCall: {
 		backgroundColor: '#205375',
 		width: 400,
-		height: 50,
+		height: 70,
 		marginTop: 10,
 		justifyContent: 'center',
 		alignItems: 'center',
