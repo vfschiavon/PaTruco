95a9032971d25ce3fd3ef6853f486532f5a03443
vfs <vischijau@gmail.com>
2023-05-25 17:48:27 -0300

    Reorganized render

--- src/Points.js
+++ src/Points.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { ImageBackground, StyleSheet, Text, TextInput, Image, View, Alert } from 'react-native';
+import { Text, TextInput, Image, View, Alert } from 'react-native';
 import * as Font from 'expo-font';
 
 import Button from '../components/Button';
+import { styles } from './styles';
 
-const backgroundImage = require('../assets/background/medium.png');
 const duckPaw = require('../assets/duck/duck-paw.png');
 const duckGif = require('../assets/duck/walking-duck.gif');
 
@@ -119,144 +119,39 @@ export default class Points extends React.Component{
 
 	render() {
 		return (
-			<ImageBackground source={backgroundImage} resizeMode="cover" style={styles.background}>
-				<Text style={styles.title}>PaTruco</Text>
-
-				<Image source={duckGif} style={styles.centerImage} />
-
-				<View style={styles.row}>
-					<View style={styles.columnLeft}>
-						<TextInput onChangeText={val => this.setName(0, val)} style={styles.input} value={this.state.teams[0].name} />
-						<Text style={styles.points}>{this.state.teams[0].points}</Text>
+			<View style={styles.background}>
+				<View style={styles.titleView}>
+					<Text style={styles.titleText}>PaTruco</Text>
+				</View>
+				<View style={styles.teamsView}>
+					<View style={styles.namePointsViewRight}>
+						<TextInput onChangeText={val => this.setName(0, val)} value={this.state.teams[0].name} style={styles.inputText}/>
+						<Text style={styles.pointsText}>{this.state.teams[0].points}</Text>
 					</View>
-
-					<View style={styles.columnRight}>
-						<TextInput onChangeText={val => this.setName(1, val)} style={styles.input} value={this.state.teams[1].name} />
-						<Text style={styles.points}>{this.state.teams[1].points}</Text>
+					<View style={styles.namePointsViewLeft }>
+						<TextInput onChangeText={val => this.setName(1, val)} value={this.state.teams[1].name} style={styles.inputText}/>
+						<Text style={styles.pointsText}>{this.state.teams[1].points}</Text>
 					</View>
 				</View>
-
-				<View style={styles.columnCenter}>
-					<View style={styles.row}>
-						<View style={styles.columnLeft}>
-							<Button text='-' func={() => this.subPoint(0)} style={styles.buttonSub} />
-							<Button
-								image={duckPaw}
-								text={this.state.turnPoints}
-								func={() => this.addPoints(0, this.state.turnPoints)}
-								style={styles.buttonAdd} />
-						</View>
-
-						<View style={styles.columnRight}>
-							<Button text='-' func={() => this.subPoint(1)} style={styles.buttonSub} />
-							<Button 
-								image = {duckPaw}
-								text={this.state.turnPoints}
-								func={() => this.addPoints(1, this.state.turnPoints)}
-								style={styles.buttonAdd}
-							/>
-						</View>
-					</View>
-
-					<View style={styles.centerButtons}>
-						<Button text={this.state.turnNextCall} func={() => this.call()} style={styles.buttonCall} />
-						{/* <Button text='Reiniciar' func={() => this.showAlertResetTurn()} style={styles.buttonReset} /> */}
-						<Button text='Reiniciar' func={() => this.resetTurn()} style={styles.buttonReset} />
-					</View>
+				<View style={styles.imageView }>
+					<Image source={duckGif} style={styles.image}/>
 				</View>
-			</ImageBackground>
+				<View style={styles.subButtonsView }>
+					<Button text='-' func={() => this.subPoint(0)} style={[styles.subButtons, styles.dropShadow]} styleText={styles.subButtonsText}/>
+					<Button text='-' func={() => this.subPoint(1)} style={[styles.subButtons, styles.dropShadow]} styleText={styles.subButtonsText}/>
+				</View>
+				<View style={styles.addButtonsView }>
+					<Button image={duckPaw} text={this.state.turnPoints} func={() => this.addPoints(0, this.state.turnPoints)} style={[styles.addButtons, styles.dropShadow]} styleText={styles.addButtonsText}/>
+					<Button image={duckPaw} text={this.state.turnPoints} func={() => this.addPoints(1, this.state.turnPoints)} style={[styles.addButtons, styles.dropShadow]} styleText={styles.addButtonsText}/>
+				</View>
+				<View style={styles.callButtonView}>
+					<Button text={this.state.turnNextCall} func={() => this.call()} style={[styles.callButton, styles.dropShadow]} styleText={styles.callButtonText}/>
+				</View>
+				<View style={styles.resetButtonView}>
+					{/* () => this.showAlertResetTurn() */}
+					<Button text='Reiniciar' func={() => this.resetTurn()} style={[styles.resetButton, styles.dropShadow]} styleText={styles.resetButtonText}/>
+				</View>
+			</View>
 		);
 	}
 }
-
-const styles = StyleSheet.create({
-	background: {
-		width: '100%',
-		height: '100%',
-	},
-	title: {
-		fontSize: 65,
-		fontFamily: 'bobaCups',
-		textAlign: 'center',
-		marginTop: 40,
-	},
-	row: {
-		flexDirection: 'row',
-		justifyContent: 'space-between',
-	},
-	columnLeft: {
-		flexDirection: 'column',
-		marginLeft: '5%',
-	},
-	columnRight: {
-		flexDirection: 'column',
-		marginRight: '5%',
-	},
-	columnCenter: {
-		flex: 1,
-		justifyContent: 'flex-end',
-	},
-	centerButtons: {
-		alignItems: 'center',
-	},
-	input: {
-		height: 'auto',
-		color: 'white',
-		textAlign: 'center',
-		maxWidth: 150,
-		alignSelf: 'center',
-		fontSize: 25,
-		marginTop: 10,
-	},
-	points: {
-		fontSize: 60,
-		color: 'white',
-		textAlign: 'center',
-	},
-	centerImage: {
-		width: '50%',
-		height: '50%',
-		resizeMode: 'contain',
-		position: 'absolute',
-		alignSelf: 'center',
-		top: '20%',
-	},
-	buttonSub: {
-		backgroundColor: '#ff4d4d',
-		borderRadius: 50,
-		width: 25,
-		height: 25,
-		justifyContent: 'center',
-		marginBottom: 5,
-		borderColor: 'black',
-		borderWidth: 2,
-	},
-	buttonAdd: {
-		borderRadius: 15,
-		width: 120,
-		height: 120,
-		marginBottom: 10,
-	},
-	buttonCall: {
-		backgroundColor: '#f1c40f',
-		width: 350,
-		height: 70,
-		justifyContent: 'center',
-		alignItems: 'center',
-		borderRadius: 10,
-		marginBottom: 10,
-		borderColor: 'black',
-		borderWidth: 3,
-	},
-	buttonReset: {
-		backgroundColor: '#efefef',
-		width: 200,
-		height: 20,
-		marginBottom: 10,
-		justifyContent: 'center',
-		alignItems: 'center',
-		borderRadius: 10,
-		borderColor: 'black',
-		borderWidth: 2,
-	},
-});
